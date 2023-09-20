3c35f859166fc0227a18b0d504a38913fe2a3eca
PabloMurilloR <pablo.murillo.retuerta@gmail.com>
2023-09-20 20:01:23 +0200

    Calculadora

---
+++ CalculadoraPablo/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace CalculadoraPablo
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
+            this.CE = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.MC = new System.Windows.Forms.Button();
+            this.Siete = new System.Windows.Forms.Button();
+            this.Ocho = new System.Windows.Forms.Button();
+            this.Nueve = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
+            this.MR = new System.Windows.Forms.Button();
+            this.Cuatro = new System.Windows.Forms.Button();
+            this.Cinco = new System.Windows.Forms.Button();
+            this.Seis = new System.Windows.Forms.Button();
+            this.Multiplicacion = new System.Windows.Forms.Button();
+            this.MS = new System.Windows.Forms.Button();
+            this.Uno = new System.Windows.Forms.Button();
+            this.Dos = new System.Windows.Forms.Button();
+            this.Tres = new System.Windows.Forms.Button();
+            this.Menos = new System.Windows.Forms.Button();
+            this.MMas = new System.Windows.Forms.Button();
+            this.Cero = new System.Windows.Forms.Button();
+            this.Division1 = new System.Windows.Forms.Button();
+            this.Mas = new System.Windows.Forms.Button();
+            this.Igual = new System.Windows.Forms.Button();
+            this.MResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CE
+            // 
+            this.CE.ForeColor = System.Drawing.Color.Red;
+            this.CE.Location = new System.Drawing.Point(174, 97);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(84, 46);
+            this.CE.TabIndex = 0;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
+            this.CE.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // C
+            // 
+            this.C.ForeColor = System.Drawing.Color.Red;
+            this.C.Location = new System.Drawing.Point(264, 97);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(84, 46);
+            this.C.TabIndex = 1;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // MC
+            // 
+            this.MC.ForeColor = System.Drawing.Color.Red;
+            this.MC.Location = new System.Drawing.Point(17, 149);
+            this.MC.Name = "MC";
+            this.MC.Size = new System.Drawing.Size(58, 63);
+            this.MC.TabIndex = 2;
+            this.MC.Text = "MC";
+            this.MC.UseVisualStyleBackColor = true;
+            this.MC.Click += new System.EventHandler(this.MC_Click);
+            // 
+            // Siete
+            // 
+            this.Siete.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Siete.Location = new System.Drawing.Point(98, 149);
+            this.Siete.Name = "Siete";
+            this.Siete.Size = new System.Drawing.Size(58, 63);
+            this.Siete.TabIndex = 3;
+            this.Siete.Text = "7";
+            this.Siete.UseVisualStyleBackColor = true;
+            this.Siete.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Ocho
+            // 
+            this.Ocho.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Ocho.Location = new System.Drawing.Point(162, 149);
+            this.Ocho.Name = "Ocho";
+            this.Ocho.Size = new System.Drawing.Size(58, 63);
+            this.Ocho.TabIndex = 4;
+            this.Ocho.Text = "8";
+            this.Ocho.UseVisualStyleBackColor = true;
+            this.Ocho.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Nueve
+            // 
+            this.Nueve.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Nueve.Location = new System.Drawing.Point(226, 149);
+            this.Nueve.Name = "Nueve";
+            this.Nueve.Size = new System.Drawing.Size(58, 63);
+            this.Nueve.TabIndex = 5;
+            this.Nueve.Text = "9";
+            this.Nueve.UseVisualStyleBackColor = true;
+            this.Nueve.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Division
+            // 
+            this.Division.ForeColor = System.Drawing.Color.Red;
+            this.Division.Location = new System.Drawing.Point(290, 149);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(58, 63);
+            this.Division.TabIndex = 6;
+            this.Division.Text = "/";
+            this.Division.UseVisualStyleBackColor = true;
+            this.Division.Click += new System.EventHandler(this.Division_Click);
+            // 
+            // MR
+            // 
+            this.MR.ForeColor = System.Drawing.Color.Red;
+            this.MR.Location = new System.Drawing.Point(17, 218);
+            this.MR.Name = "MR";
+            this.MR.Size = new System.Drawing.Size(58, 63);
+            this.MR.TabIndex = 7;
+            this.MR.Text = "MR";
+            this.MR.UseVisualStyleBackColor = true;
+            this.MR.Click += new System.EventHandler(this.MR_Click);
+            // 
+            // Cuatro
+            // 
+            this.Cuatro.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Cuatro.Location = new System.Drawing.Point(98, 218);
+            this.Cuatro.Name = "Cuatro";
+            this.Cuatro.Size = new System.Drawing.Size(58, 63);
+            this.Cuatro.TabIndex = 8;
+            this.Cuatro.Text = "4";
+            this.Cuatro.UseVisualStyleBackColor = true;
+            this.Cuatro.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Cinco
+            // 
+            this.Cinco.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Cinco.Location = new System.Drawing.Point(162, 218);
+            this.Cinco.Name = "Cinco";
+            this.Cinco.Size = new System.Drawing.Size(58, 63);
+            this.Cinco.TabIndex = 9;
+            this.Cinco.Text = "5";
+            this.Cinco.UseVisualStyleBackColor = true;
+            this.Cinco.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Seis
+            // 
+            this.Seis.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Seis.Location = new System.Drawing.Point(226, 218);
+            this.Seis.Name = "Seis";
+            this.Seis.Size = new System.Drawing.Size(58, 63);
+            this.Seis.TabIndex = 10;
+            this.Seis.Text = "6";
+            this.Seis.UseVisualStyleBackColor = true;
+            this.Seis.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Multiplicacion
+            // 
+            this.Multiplicacion.ForeColor = System.Drawing.Color.Red;
+            this.Multiplicacion.Location = new System.Drawing.Point(290, 218);
+            this.Multiplicacion.Name = "Multiplicacion";
+            this.Multiplicacion.Size = new System.Drawing.Size(58, 63);
+            this.Multiplicacion.TabIndex = 11;
+            this.Multiplicacion.Text = "*";
+            this.Multiplicacion.UseVisualStyleBackColor = true;
+            this.Multiplicacion.Click += new System.EventHandler(this.Multiplicacion_Click);
+            // 
+            // MS
+            // 
+            this.MS.ForeColor = System.Drawing.Color.Red;
+            this.MS.Location = new System.Drawing.Point(17, 287);
+            this.MS.Name = "MS";
+            this.MS.Size = new System.Drawing.Size(58, 63);
+            this.MS.TabIndex = 12;
+            this.MS.Text = "MS";
+            this.MS.UseVisualStyleBackColor = true;
+            this.MS.Click += new System.EventHandler(this.MS_Click);
+            // 
+            // Uno
+            // 
+            this.Uno.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Uno.Location = new System.Drawing.Point(98, 287);
+            this.Uno.Name = "Uno";
+            this.Uno.Size = new System.Drawing.Size(58, 63);
+            this.Uno.TabIndex = 13;
+            this.Uno.Text = "1";
+            this.Uno.UseVisualStyleBackColor = true;
+            this.Uno.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Dos
+            // 
+            this.Dos.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Dos.Location = new System.Drawing.Point(162, 287);
+            this.Dos.Name = "Dos";
+            this.Dos.Size = new System.Drawing.Size(58, 63);
+            this.Dos.TabIndex = 14;
+            this.Dos.Text = "2";
+            this.Dos.UseVisualStyleBackColor = true;
+            this.Dos.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Tres
+            // 
+            this.Tres.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Tres.Location = new System.Drawing.Point(226, 287);
+            this.Tres.Name = "Tres";
+            this.Tres.Size = new System.Drawing.Size(58, 63);
+            this.Tres.TabIndex = 15;
+            this.Tres.Text = "3";
+            this.Tres.UseVisualStyleBackColor = true;
+            this.Tres.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Menos
+            // 
+            this.Menos.ForeColor = System.Drawing.Color.Red;
+            this.Menos.Location = new System.Drawing.Point(290, 287);
+            this.Menos.Name = "Menos";
+            this.Menos.Size = new System.Drawing.Size(58, 63);
+            this.Menos.TabIndex = 16;
+            this.Menos.Text = "-";
+            this.Menos.UseVisualStyleBackColor = true;
+            this.Menos.Click += new System.EventHandler(this.Menos_Click);
+            // 
+            // MMas
+            // 
+            this.MMas.ForeColor = System.Drawing.Color.Red;
+            this.MMas.Location = new System.Drawing.Point(17, 356);
+            this.MMas.Name = "MMas";
+            this.MMas.Size = new System.Drawing.Size(58, 63);
+            this.MMas.TabIndex = 17;
+            this.MMas.Text = "M+";
+            this.MMas.UseVisualStyleBackColor = true;
+            this.MMas.Click += new System.EventHandler(this.MMas_Click);
+            // 
+            // Cero
+            // 
+            this.Cero.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Cero.Location = new System.Drawing.Point(98, 356);
+            this.Cero.Name = "Cero";
+            this.Cero.Size = new System.Drawing.Size(58, 63);
+            this.Cero.TabIndex = 18;
+            this.Cero.Text = "0";
+            this.Cero.UseVisualStyleBackColor = true;
+            this.Cero.Click += new System.EventHandler(this.BtonNumeros);
+            // 
+            // Division1
+            // 
+            this.Division1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.Division1.Location = new System.Drawing.Point(162, 356);
+            this.Division1.Name = "Division1";
+            this.Division1.Size = new System.Drawing.Size(58, 63);
+            this.Division1.TabIndex = 19;
+            this.Division1.Text = "1/x";
+            this.Division1.UseVisualStyleBackColor = true;
+            this.Division1.Click += new System.EventHandler(this.Division1_Click);
+            // 
+            // Mas
+            // 
+            this.Mas.ForeColor = System.Drawing.Color.Red;
+            this.Mas.Location = new System.Drawing.Point(226, 356);
+            this.Mas.Name = "Mas";
+            this.Mas.Size = new System.Drawing.Size(58, 63);
+            this.Mas.TabIndex = 20;
+            this.Mas.Text = "+";
+            this.Mas.UseVisualStyleBackColor = true;
+            this.Mas.Click += new System.EventHandler(this.Mas_Click);
+            // 
+            // Igual
+            // 
+            this.Igual.ForeColor = System.Drawing.Color.Red;
+            this.Igual.Location = new System.Drawing.Point(290, 356);
+            this.Igual.Name = "Igual";
+            this.Igual.Size = new System.Drawing.Size(58, 63);
+            this.Igual.TabIndex = 21;
+            this.Igual.Text = "=";
+            this.Igual.UseVisualStyleBackColor = true;
+            this.Igual.Click += new System.EventHandler(this.Igual_Click);
+            // 
+            // MResultado
+            // 
+            this.MResultado.Enabled = false;
+            this.MResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MResultado.Location = new System.Drawing.Point(17, 16);
+            this.MResultado.Multiline = true;
+            this.MResultado.Name = "MResultado";
+            this.MResultado.Size = new System.Drawing.Size(331, 63);
+            this.MResultado.TabIndex = 22;
+            this.MResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.MResultado.TextChanged += new System.EventHandler(this.MResultado_TextChanged);
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 439);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.MResultado);
+            this.Controls.Add(this.Igual);
+            this.Controls.Add(this.Mas);
+            this.Controls.Add(this.Division1);
+            this.Controls.Add(this.Cero);
+            this.Controls.Add(this.MMas);
+            this.Controls.Add(this.Menos);
+            this.Controls.Add(this.Tres);
+            this.Controls.Add(this.Dos);
+            this.Controls.Add(this.Uno);
+            this.Controls.Add(this.MS);
+            this.Controls.Add(this.Multiplicacion);
+            this.Controls.Add(this.Seis);
+            this.Controls.Add(this.Cinco);
+            this.Controls.Add(this.Cuatro);
+            this.Controls.Add(this.MR);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Nueve);
+            this.Controls.Add(this.Ocho);
+            this.Controls.Add(this.Siete);
+            this.Controls.Add(this.MC);
+            this.Controls.Add(this.C);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Calculadora";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Calculadora_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button MC;
+        private System.Windows.Forms.Button Siete;
+        private System.Windows.Forms.Button Ocho;
+        private System.Windows.Forms.Button Nueve;
+        private System.Windows.Forms.Button Division;
+        private System.Windows.Forms.Button MR;
+        private System.Windows.Forms.Button Cuatro;
+        private System.Windows.Forms.Button Cinco;
+        private System.Windows.Forms.Button Seis;
+        private System.Windows.Forms.Button Multiplicacion;
+        private System.Windows.Forms.Button MS;
+        private System.Windows.Forms.Button Uno;
+        private System.Windows.Forms.Button Dos;
+        private System.Windows.Forms.Button Tres;
+        private System.Windows.Forms.Button Menos;
+        private System.Windows.Forms.Button MMas;
+        private System.Windows.Forms.Button Cero;
+        private System.Windows.Forms.Button Division1;
+        private System.Windows.Forms.Button Mas;
+        private System.Windows.Forms.Button Igual;
+        private System.Windows.Forms.TextBox MResultado;
+    }
+}
+
---
+++ CalculadoraPablo/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraPablo
+{
+    public partial class Calculadora : Form
+    {
+        String texto = "";
+        double num1;
+        double num2;
+        double memoria = 0;
+        string operador = "";
+
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Botones
+
+        #region Numeros
+        private void BtonNumeros(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            if (bt.Name.Equals("Cero"))
+            {
+                MResultado.Text += 0;
+                texto += "0";
+            } else if (bt.Name.Equals("Uno"))
+            {
+                MResultado.Text += 1;
+                texto += "1";
+            } else if (bt.Name.Equals("Dos"))
+            {
+                MResultado.Text += 2;
+                texto += "2";
+            } else if (bt.Name.Equals("Tres"))
+            {
+                MResultado.Text += 3;
+                texto += "3";
+            } else if (bt.Name.Equals("Cuatro"))
+            {
+                MResultado.Text += 4;
+                texto += "4";
+            } else if (bt.Name.Equals("Cinco"))
+            {
+                MResultado.Text += 5;
+                texto += "5";
+            } else if (bt.Name.Equals("Seis"))
+            {
+                MResultado.Text += 6;
+                texto += "6";
+            } else if (bt.Name.Equals("Siete"))
+            {
+                MResultado.Text += 7;
+                texto += "7";
+            } else if (bt.Name.Equals("Ocho"))
+            {
+                MResultado.Text += 8;
+                texto += "8";
+            } else if (bt.Name.Equals("Nueve"))
+            {
+                MResultado.Text += 9;
+                texto += "9";
+            }
+        }
+        #endregion
+
+        #region Borrar
+        private void CE_Click(object sender, EventArgs e)
+        {
+            if (texto.Substring(0, texto.Length - 1).Length > 0)
+            {
+                texto = texto.Substring(0, texto.Length - 1);
+                MResultado.Text = texto;
+            } else
+            {
+                MResultado.Text = "";
+            }
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            MResultado.Text = "";
+            texto = "";
+        }
+        #endregion
+
+
+        #region Operaciones
+        private void Division1_Click(object sender, EventArgs e)
+        {
+            if (texto.Equals("0"))
+            {
+                MResultado.Text = "";
+                MessageBox.Show("La division entre 0 no es válida");
+            } else
+            {
+                double resultado = 1 / double.Parse(texto);
+                MResultado.Text = resultado.ToString();
+            }
+        }
+
+        private void Multiplicacion_Click(object sender, EventArgs e)
+        {
+            MResultado.Text = "";
+            num1 = double.Parse(texto);
+            texto = "";
+            operador = "*";
+        }
+
+        private void Division_Click(object sender, EventArgs e)
+        {
+            MResultado.Text = "";
+            num1 = double.Parse(texto);
+            texto = "";
+            operador = "/";
+        }
+
+        private void Mas_Click(object sender, EventArgs e)
+        {
+            MResultado.Text = "";
+            num1 = double.Parse(texto);
+            texto = "";
+            operador = "+";
+        }
+
+        private void Menos_Click(object sender, EventArgs e)
+        {
+            MResultado.Text = "";
+            num1 = double.Parse(texto);
+            texto = "";
+            operador = "-";
+        }
+
+        private void Igual_Click(object sender, EventArgs e)
+        {
+            num2 = double.Parse(texto);
+            if (operador.Equals("/"))
+            {
+                if(num2 == 0)
+                {
+                    MResultado.Text = "";
+                    MessageBox.Show("La division entre 0 no es válida");
+                } else
+                {
+                    MResultado.Text =  (num1 / num2).ToString();
+                }
+            } else if (operador.Equals("*"))
+            {
+                MResultado.Text = (num1 * num2).ToString();
+            } else if (operador.Equals("+"))
+            {
+                MResultado.Text = (num1 + num2).ToString();
+            } else if (operador.Equals("-"))
+            {
+                MResultado.Text = (num1 - num2).ToString();
+            }
+
+        }
+        #endregion
+
+        #region Memoria
+        private void MC_Click(object sender, EventArgs e)
+        {
+            memoria = 0;
+        }
+
+        private void MR_Click(object sender, EventArgs e)
+        {
+            MResultado.Text = memoria.ToString();
+        }
+        private void MS_Click(object sender, EventArgs e)
+        {
+            memoria = double.Parse(texto);
+        }
+
+        private void MMas_Click(object sender, EventArgs e)
+        {
+            memoria += double.Parse(texto);
+        }
+        #endregion
+
+        #endregion
+
+    }
+}
