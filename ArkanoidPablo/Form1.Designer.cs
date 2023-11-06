e1b98c4e51ec723cc24e38521423244d92d5b7b3
PabloMurilloR <pablo.murillo.retuerta@gmail.com>
2023-11-06 21:16:45 +0100

    Primer dia arkanoid, solo imagenes añadidas

---
+++ ArkanoidPablo/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ArkanoidPablo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.barra = new System.Windows.Forms.PictureBox();
+            this.pelota = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barra)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pelota)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 492);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // barra
+            // 
+            this.barra.BackColor = System.Drawing.Color.Transparent;
+            this.barra.Image = ((System.Drawing.Image)(resources.GetObject("barra.Image")));
+            this.barra.Location = new System.Drawing.Point(182, 419);
+            this.barra.Name = "barra";
+            this.barra.Size = new System.Drawing.Size(94, 25);
+            this.barra.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.barra.TabIndex = 1;
+            this.barra.TabStop = false;
+            // 
+            // pelota
+            // 
+            this.pelota.BackColor = System.Drawing.Color.Transparent;
+            this.pelota.Image = ((System.Drawing.Image)(resources.GetObject("pelota.Image")));
+            this.pelota.Location = new System.Drawing.Point(215, 388);
+            this.pelota.Name = "pelota";
+            this.pelota.Size = new System.Drawing.Size(29, 25);
+            this.pelota.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pelota.TabIndex = 2;
+            this.pelota.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(454, 491);
+            this.Controls.Add(this.pelota);
+            this.Controls.Add(this.barra);
+            this.Controls.Add(this.pictureBox1);
+            this.Enabled = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barra)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pelota)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox barra;
+        private System.Windows.Forms.PictureBox pelota;
+    }
+}
+
