543b5d00eeb38ae1b057648646bd89105cb7a963
PabloMurilloR <pablo.murillo.retuerta@gmail.com>
2023-11-10 19:44:11 +0100

    Reinicio pelota correctamente

--- ArkanoidPablo/Form1.cs
+++ ArkanoidPablo/Form1.cs
@@ -14,8 +14,9 @@ namespace ArkanoidPablo
 {
     public partial class Arkanoid : Form
     {
-        private int velocidad = 10; // Velocidad del movimiento
-        private int angulacion = 5;
+        int posX;
+        private int velocidad = 30; // Velocidad del movimiento
+        private int angulacion = 10;
 
         public Arkanoid()
         {
@@ -34,9 +35,10 @@ namespace ArkanoidPablo
 
         private void IniciarMovimiento_Click(object sender, EventArgs e)
         {
-            pelota.Location = new System.Drawing.Point(pelota.Location.X, pelota.Location.Y);
+            pelota.Location = new Point(pelota.Location.X, pelota.Location.Y);
 
         }
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Space)
@@ -49,15 +51,21 @@ namespace ArkanoidPablo
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            int posX = pelota.Location.X;
+            posX = pelota.Location.X;
             int posY = pelota.Location.Y;
             posX += velocidad;
-            //posY += velocidad;
 
             if (posX + pelota.Width > this.Width || posX < 0)
             {
                 // Cambia la dirección al llegar a un límite
                 velocidad = -velocidad;
+            }
+            else if (posY >= 450)
+            {
+                angulacion = 0;
+                velocidad = 0;
+                Console.WriteLine("Abajo");
+                reinicio();
             }
             else if (posY + pelota.Width > this.Width || posY < 0)
             {
@@ -65,8 +73,18 @@ namespace ArkanoidPablo
             }
             pelota.Invalidate();
             // Actualiza la posición del PictureBox
-            pelota.Location = new System.Drawing.Point(posX, pelota.Location.Y - angulacion);
+            pelota.Location = new Point(posX, pelota.Location.Y - angulacion);
 
+        }
+
+        public void reinicio()
+        {
+            posX = barra1.Location.X + (barra1.Width/2) - 10; 
+            timer1.Stop();
+            pelota.Invalidate();
+            pelota.Location = new Point(posX, 398);
+           // this.KeyPreview = true;
+            Console.WriteLine("Reinicio");
         }
     }
 }
